0bb861cda6d9dafefe8ba65ebbb874f9cea8c26a
Peter Moelzer <moelzerpeter@gmail.com>
2026-06-17 14:27:16 +0200

    cleaning up

--- test_btn.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-root = tk.Tk()
-def cb(): print('Clicked!')
-btn = ttk.Button(root, text='Base Monthly Income:', command=cb)
-btn.pack()
-root.update()
-print('State:', btn.state())
